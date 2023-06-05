a4dc26604952359c89d411addd5beeef6e7c75dd
Vikram Solanki <134689573+solanki1750@users.noreply.github.com>
2023-06-05 23:35:19 +0530

    Update 16. Decorator.py

--- 16. Decorator.py
+++ 16. Decorator.py
@@ -43,3 +43,80 @@ h1('Another headline')
 p = html_tag('p')
 p('Paragraph 1')
 
+
+### CLOSURE
+# using argumnet
+def outer_func():
+    message = 'Hi'
+    def inner_func():
+        print(message)
+   #1 return inner_func() # executing the inner function so, it will return print(message) which is Hi
+    return inner_func  # we are creating a function, but not executing it, so it will return function
+#1 outer_func() # this would have returned print Hi from #1
+my_func = outer_func()  # this is a function which is equal to inner_func
+print(my_func.__name__)
+my_func() # now we are executing inner function
+my_func() # inner function executed twice, although outer function is executed only once
+my_func() # inner function executed 3 times, although outer function is executed only once
+
+# using parameter
+def outer_func(msg):
+    message = msg
+    def inner_func():
+        print(message)
+    return inner_func
+
+hi_func = outer_func('Hi')
+hello_func = outer_func('Hello')
+
+hi_func()
+hello_func()
+
+# simplified
+def outer_func(msg):
+    def inner_func():
+        print(msg)
+    return inner_func
+
+hi_func = outer_func('Hi')
+hello_func = outer_func('Hello')
+
+hi_func()
+hello_func()
+
+#  closure is INNER function that remembers and has access to variables in local scope in which it was created.
+#  even after outer function finished executing.
+
+
+##### DECORATOR
+# simple decorator
+def decorator_func(original_func):  # passing function as parameter
+    def wrapper_func():
+        print('Wrapper function executed before {}'.format(original_func.__name__))
+        return original_func()  # executing parameter function and returning output
+
+    return wrapper_func
+
+def display():
+    print('This is parameter function, it will be executed in wrapper function')
+
+decorated_display_variable = decorator_func(display)
+decorated_display_variable() # when wrapper function is executed, it in truns executes the display function.
+
+
+# @ decorator
+def decorator_func(original_func):  # passing function as parameter
+    def wrapper_func():
+        print('Wrapper function executed before {}'.format(original_func.__name__))
+        return original_func()  # executing parameter function and returning output
+    return wrapper_func
+
+@decorator_func  # function will be passed as input to decorator_func and then its fate lies there
+def display():
+    print('This is parameter function, it will be executed in wrapper function')
+
+display() # it first goes inside decorator_func and then get executed there.
+# if we remove the @decorated function then display function will execute and decorator_func will not execute
+
+
+

2f67cdc901ba9e2a98cb1c643bc369dc4e35442d
Vikram Solanki <134689573+solanki1750@users.noreply.github.com>
2023-06-27 11:19:36 +0530

    Update Executor.py

--- Executor.py
+++ Executor.py
@@ -1,6 +1,7 @@
 # duck typing = concept where the class of an object is less important than the methods/attributes
 #               class type is not checked if minimum methods/attributes are present
 #               “If it walks like a duck, and it quacks like a duck, then it must be a duck.”
+# hellow duck
 
 class Duck:
     def walk(self):
